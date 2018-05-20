65a1351aaf11da3d6f72e887daba3db74b195c13
Nam Pham <nampdn@protonmail.com>
2018-05-21 05:20:07 +0700

    build(yarn): detach start scripts for dev/prod environment
    it's required that every service has two scripts `start:dev` and `start:prod`

--- tools/yarn.js
+++ tools/yarn.js
@@ -2,4 +2,9 @@ const cp = require('child_process')
 
 console.log('Checking dependencies...')
 cp.spawnSync('yarn', ['install', '--no-progress'], { stdio: 'inherit' })
-cp.spawnSync('yarn', ['start'], { stdio: 'inherit' })
+
+if (process.env.NODE_ENV === 'production') {
+  cp.spawnSync('yarn', ['start:prod'], { stdio: 'inherit' })
+} else {
+  cp.spawnSync('yarn', ['start:dev'], { stdio: 'inherit' })
+}
