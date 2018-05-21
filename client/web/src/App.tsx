6594f9e5a3091d724ae3f76cc2ae9a17819889da
Nam Pham <nampdn@protonmail.com>
2018-05-21 15:57:13 +0700

    refactor(web): switch to typescript project

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Typescript</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
     );
