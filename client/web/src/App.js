9023bbfc5e30b7b87e8e17fc1820b60326604816
Nam Pham <nampdn@protonmail.com>
2018-05-23 23:33:04 +0700

    feat(web): update Auth form by taking advantage of render props

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
 import Auth from './components/auth'
 
 class App extends Component {
@@ -14,10 +14,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Auth header="Hello World!" />
+        <Auth
+          email="admin@phuc.am"
+          password="nooneknows"
+          onLogin={isAuthenticating => console.log(`Authenticating: ${isAuthenticating}`)}
+          render={auth => (
+            <form>
+              <input type="email" value={auth.email} onChange={auth.updateEmail} />
+              <input type="password" value={auth.password} onChange={auth.updatePassword} />
+              <hr />
+              <button type="button" onClick={auth.reset}>
+                Reset
+              </button>
+              <button type="button" onClick={auth.login}>
+                Login
+              </button>
+            </form>
+          )}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- client/web/src/components/auth/Auth.js
+++ client/web/src/components/auth/Auth.js
@@ -1,30 +1,55 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDom from 'react-dom'
-import * as p from 'react-primitives'
-import styled from 'styled-components/primitives'
-
-const Container = styled.View`
-  width: 100%;
-`
-
-const Header = styled.Text`
-  color: #9c9c9c;
-`
 
 /**
  * @render react
  * @name Auth
  * @description Authentication form.
  * @example
- * <Auth header={"Authenticate"}>
+ * <Auth>
  * </Auth>
  */
-const Auth = ({header}) => {
-  return (
-    <Container>
-      <Header>{header}</Header>
-    </Container>
-  )
+class Auth extends Component {
+  static defaultProps = {
+    email: '',
+    password: '',
+    isAuthenticating: false,
+    onLogin: () => {},
+  }
+
+  initialState = {
+    email: this.props.email,
+    password: this.props.password,
+    isAuthenticating: this.props.isAuthenticating,
+  }
+
+  state = this.initialState
+
+  reset = () => this.setState(this.initialState)
+  login = () =>
+    this.setState(
+      ({isAuthenticating}) => ({isAuthenticating: !isAuthenticating}),
+      () => this.props.onLogin(this.state.isAuthenticating),
+    )
+  updateEmail = event => this.setState({email: event.target.value})
+  updatePassword = event => this.setState({password: event.target.value})
+
+  getAuthProps = ({...props} = {}) => ({
+    ...props,
+  })
+
+  render() {
+    return this.props.render({
+      email: this.state.email,
+      password: this.state.password,
+      isAuthenticating: this.state.isAuthenticating,
+      reset: this.reset,
+      login: this.login,
+      updateEmail: this.updateEmail,
+      updatePassword: this.updatePassword,
+      getAuthProps: this.getAuthProps,
+    })
+  }
 }
 
 export default Auth
