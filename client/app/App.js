da015aa99484e275010bb0fd33c34e90c91da5a6
Nam Pham <nampdn@protonmail.com>
2018-05-24 14:24:24 +0700

    chore(app): remove @bit component for travis build testing

--- client/app/App.js
+++ client/app/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import Auth from '@bit/writeforchrist.vistart.components.auth'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Auth email="admin@phuc.am"
-          password="nooneknows"
-          render={(auth) => (
-            <View>
-              <TextInput value={auth.email} onChange={auth.updateEmail} />
-              <TextInput value={auth.password} onChange={auth.updatePassword} />
-              <TouchableOpacity onPress={auth.login}>
-                <Text>Login</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={auth.reset}>
-                <Text>Reset</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-        />
       </View>
     );
   }
