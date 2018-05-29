a5ca75118f0094d260f0548de51f940af8165fdc
Nam Pham <nampdn@protonmail.com>
2018-05-29 13:59:27 +0700

    chore(api): add PORT env

--- server/api/config/index.ts
+++ server/api/config/index.ts
@@ -1,2 +1,3 @@
-export const GRAPH_ENDPOINT = process.env.GRAPH_ENDPOINT
-export const APP_SECRET = process.env.APP_SECRET
+export const PORT = process.env.PORT || 4002
+export const GRAPH_ENDPOINT = process.env.GRAPH_ENDPOINT || 'http://server_graph:4000'
+export const APP_SECRET = process.env.APP_SECRET || ''
