94cf6bed21f66c0dc5a85d846c9a5f653951a4d0
Nam Pham <nampdn@protonmail.com>
2018-05-30 15:56:18 +0700

    feat(graph): add facebook auth mutation

--- server/graph/src/resolvers/Mutation/auth.ts
+++ server/graph/src/resolvers/Mutation/auth.ts
@@ -1,22 +1,23 @@
 import * as bcrypt from 'bcryptjs'
 import * as jwt from 'jsonwebtoken'
-import { Context } from '../../utils'
+import {Context} from '../../utils'
 
 export const auth = {
   async signup(parent, args, ctx: Context, info) {
+    const {email, name} = args
     const password = await bcrypt.hash(args.password, 10)
     const user = await ctx.db.mutation.createUser({
-      data: { ...args, password },
+      data: {email, password, profile: {create: {name}}},
     })
 
     return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+      token: jwt.sign({userId: user.id}, process.env.APP_SECRET),
       user,
     }
   },
 
-  async login(parent, { email, password }, ctx: Context, info) {
-    const user = await ctx.db.query.user({ where: { email } })
+  async login(parent, {email, password}, ctx: Context, info) {
+    const user = await ctx.db.query.user({where: {email}})
     if (!user) {
       throw new Error(`No such user found for email: ${email}`)
     }
@@ -27,7 +28,19 @@ export const auth = {
     }
 
     return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+      token: jwt.sign({userId: user.id}, process.env.APP_SECRET),
+      user,
+    }
+  },
+
+  async authFacebook(parent, {facebook, email, name, picture}, ctx: Context, info) {
+    const initPassword = email + facebook
+    const password = await bcrypt.hash(initPassword, 10)
+    const user = await ctx.db.mutation.createUser({
+      data: {facebook, email, profile: {create: {name, picture}}, password},
+    })
+    return {
+      token: jwt.sign({userId: user.id}, process.env.APP_SECRET),
       user,
     }
   },
