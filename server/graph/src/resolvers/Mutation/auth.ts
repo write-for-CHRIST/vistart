df4e9b5bc16b1f129daa402b29c4ebb75bf7da08
Nam Pham <nampdn@protonmail.com>
2018-05-31 22:14:14 +0700

    feat(graph): add facebook authentication mutation
    this is in expirement state
    
    close #6

--- server/graph/src/resolvers/Mutation/auth.ts
+++ server/graph/src/resolvers/Mutation/auth.ts
@@ -1,22 +1,127 @@
 import * as bcrypt from 'bcryptjs'
 import * as jwt from 'jsonwebtoken'
+import {GraphQLResolveInfo} from 'graphql'
+import {
+  AuthProvider,
+  Prisma,
+  ProfileCreateOneWithoutUserInput,
+  ProfileCreateWithoutUserInput,
+  User,
+  UserCreateWithoutPostsInput,
+  UserWhereUniqueInput,
+} from '../../generated/prisma'
 import {Context} from '../../utils'
+
+const FACEBOOK_PROVIDER = 'FACEBOOK'
+const GOOGLE_PROVIDER = 'GOOGLE'
 
 //#region Interfaces
 interface IDefaultPwdParams {
   email: string
   salt: string
 }
+interface IDatabase {
+  db: Prisma
+}
+interface IProviderPayload {
+  id: string
+  accessToken: string
+}
+interface IFacebookPayload extends IProviderPayload {
+  email: string
+  name: string
+  picture: string
+}
+interface IFindUserParams extends IDatabase {
+  where: UserWhereUniqueInput
+}
+interface ILinkParams extends IDatabase {
+  user: User
+  payload: IProviderPayload
+}
+interface ILinkProviderParams extends IDatabase {
+  user: User
+  provider: AuthProvider
+  payload: IProviderPayload
+}
+interface ICreateProviderParams extends IDatabase {
+  provider: AuthProvider
+  payload: IFacebookPayload
+}
 //#endregion
 
-//#region Functions
-const getDefaultPwd = ({email, salt}: IDefaultPwdParams) => {
-  return `${email}+${salt}`
+//#region Non-data Functions
+const hashPassword = async (pwd: string) => await bcrypt.hash(pwd, 10)
+const getDefaultPwd = ({email, salt}: IDefaultPwdParams) => `${email}+${salt}`
+const getAuthData = ({user}) => ({token: generateJWT({user}), user})
+const generateJWT = ({user}) => jwt.sign({userId: user.id}, process.env.APP_SECRET)
+const buildProfile = (
+  profile: ProfileCreateWithoutUserInput,
+): ProfileCreateOneWithoutUserInput => ({create: {...profile}})
+//#endregion
+
+//#region Binding Functions
+const findUser = async ({db: {query}, where}: IFindUserParams): Promise<User | null> =>
+  query.user({where})
+
+const linkUserWithFacebook = ({
+  db: {
+    mutation: {updateUser},
+  },
+  user: {email},
+  payload: {id: facebook, accessToken},
+}: ILinkParams): Promise<User | null> => {
+  const create = {kind: <AuthProvider>FACEBOOK_PROVIDER, accessToken}
+  const where = {email}
+  const data = {facebook, tokens: {create}}
+  return updateUser({where, data})
 }
-const generateToken = ({user}) => {
-  return jwt.sign({userId: user.id}, process.env.APP_SECRET)
+
+const createUserWithFacebook = async ({
+  db: {
+    mutation: {createUser},
+  },
+  payload: {id: facebook, name, email, picture, accessToken},
+  provider,
+}: ICreateProviderParams) => {
+  const newUser: UserCreateWithoutPostsInput = {
+    email,
+    password: await hashPassword(getDefaultPwd({email, salt: facebook})),
+    profile: buildProfile({name, picture}),
+    tokens: {create: [{kind: FACEBOOK_PROVIDER, accessToken}]},
+  }
+  return createUser({data: newUser})
 }
-const getAuthData = ({user}) => ({token: generateToken({user}), user})
+
+const linkUserWithGoogle = async ({
+  db: {mutation},
+  user,
+  payload,
+}: ILinkParams): Promise<User | null> => {
+  return null
+}
+
+const linkProvider = ({db, user, payload, provider}: ILinkProviderParams): Promise<User | null> => {
+  const linkParams: ILinkParams = {db, user, payload}
+  switch (provider) {
+    case FACEBOOK_PROVIDER:
+      return linkUserWithFacebook(linkParams)
+    case GOOGLE_PROVIDER:
+      return linkUserWithGoogle(linkParams)
+    default:
+      return null
+  }
+}
+
+const newProvider = ({db, provider, payload}: ICreateProviderParams): Promise<User | null> => {
+  const createParams = {db, provider, payload}
+  switch (provider) {
+    case FACEBOOK_PROVIDER:
+      return createUserWithFacebook(createParams)
+    default:
+      return null
+  }
+}
 //#endregion
 
 export const auth = {
@@ -44,26 +149,27 @@ export const auth = {
     return getAuthData({user})
   },
 
-  async authFacebook(parent, {facebook, email, name, picture, accessToken}, ctx: Context, info) {
+  async authFacebook(parent, args, {db}: Context, info) {
+    const provider = FACEBOOK_PROVIDER
+    const {facebook, email, name, picture, accessToken} = args
+    const payload: IFacebookPayload = {id: facebook, ...args}
     // Check if a user with this facebook id already existed
-    const existedFBUser = await ctx.db.query.user({where: {facebook}})
+    let user = null
+    const existedFBUser = await findUser({db, where: {facebook}})
     if (existedFBUser) {
-      return getAuthData({user: existedFBUser})
+      user = existedFBUser
     } else {
       // Link with local account
-      const existedLocalUser = await ctx.db.query.user({where: {email}})
-      if (existedLocalUser) {
-        const updatedUser = await ctx.db.mutation.updateUser({where: {email}, data: {facebook}})
-        return getAuthData({user: existedLocalUser})
+      const localUser = await findUser({db, where: {email}})
+      if (localUser) {
+        const linkedUser = await linkProvider({db, user: localUser, payload, provider})
+        user = linkedUser
       } else {
         // Create new account
-        const initPassword = email + facebook
-        const password = await bcrypt.hash(initPassword, 10)
-        const user = await ctx.db.mutation.createUser({
-          data: {facebook, email, profile: {create: {name, picture}}, password},
-        })
-        return getAuthData({user})
+        const createdUser = await newProvider({db, payload, provider})
+        user = createdUser
       }
     }
+    return getAuthData({user})
   },
 }
