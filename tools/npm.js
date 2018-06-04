ecaa21dd7614fc0f3967ea24aa2c7eca2a3dc055
Nam Pham <nampdn@protonmail.com>
2018-06-04 19:37:20 +0700

    fix(tools): revert back to origin sync spawn

--- tools/npm.js
+++ tools/npm.js
@@ -1,15 +1,5 @@
 const cp = require('child_process')
 
 console.log('Checking dependencies...')
-const start = async () => {
-  return new Promise((resolve, reject) => {
-    const npm = cp.spawn('npm', ['install'], {stdio: 'inherit'})
-    npm.on('close', code => {
-      code === 0 ? resolve() : reject()
-    })
-  })
-}
-
-start()
-  .then(cp.spawnSync('npm', ['start'], {stdio: 'inherit'}))
-  .catch(err => console.error(err))
+cp.spawnSync('npm', ['install'], {stdio: 'inherit'})
+cp.spawnSync('npm', ['start'], {stdio: 'inherit'})
