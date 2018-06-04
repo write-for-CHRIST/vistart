9f55c0c387d39f6f92fa5f0724dc11959932199b
Nam Pham <nampdn@protonmail.com>
2018-06-04 18:03:26 +0700

    build(app): fix docker build

--- tools/npm.js
+++ tools/npm.js
@@ -1,5 +1,15 @@
 const cp = require('child_process')
 
 console.log('Checking dependencies...')
-cp.spawnSync('npm', ['install', '--quiet'], { stdio: 'inherit' })
-cp.spawnSync('npm', ['start'], { stdio: 'inherit' })
+const start = async () => {
+  return new Promise((resolve, reject) => {
+    const npm = cp.spawn('npm', ['install'], {stdio: 'inherit'})
+    npm.on('close', code => {
+      code === 0 ? resolve() : reject()
+    })
+  })
+}
+
+start()
+  .then(cp.spawnSync('npm', ['start'], {stdio: 'inherit'}))
+  .catch(err => console.error(err))
